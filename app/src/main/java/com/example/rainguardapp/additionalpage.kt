34e27163f9cfe86b54ca279f7b4ac8b3ed3aeaf0
Volodymyr <volodimirbanyas@gmail.com>
2023-03-26 14:54:49 +0300

    create additional page

---
+++ app/src/main/java/com/example/rainguardapp/additionalpage.kt
@@ -0,0 +1,12 @@
+package com.example.rainguardapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class additionalpage: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.additionalpage)
+    }
+
+}
