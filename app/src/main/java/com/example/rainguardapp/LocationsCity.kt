525c085fa0cb59e0d7e8f28f7408d924e04a476c
Piwo2L <teslevych.mykhailo@student.uzhnu.edu.ua>
2023-03-29 21:22:10 +0300

    FixLocationsOB

--- app/src/main/java/com/example/rainguardapp/LocationsCity.kt
+++ app/src/main/java/com/example/rainguardapp/LocationsCity.kt
@@ -1,11 +1,10 @@
 package com.example.rainguardapp
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
-class MainActivity : AppCompatActivity() {
+class LocationsCity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.locations_city)
     }
+
 }
--- app/src/main/java/com/example/rainguardapp/LocationsOB.kt
+++ app/src/main/java/com/example/rainguardapp/LocationsOB.kt
@@ -1,4 +1,28 @@
 package com.example.rainguardapp
 
-class LocationsOB {
-}
+import android.R
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LocationsOB:AppCompatActivity(){
+        var ob = arrayOf("Автономна Республіка Крим", "Вінницька", "Волинська", "Дніпропетровська", "Донецька", "Житомирська", "Закарпатська", "Запорізька", "Івано-Франківська", "Київська", "Кіровоградська", "Луганська", "Львівська", "Миколаївська", "Одеська", "Полтавська", "Рівненська", "Сумська", "Тернопільська", "Харківська", "Херсонська", "Хмельницька", "Черкаська", "Чернівецька", "Чернігівська")
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(com.example.rainguardapp.R.layout.locations_ob)
+            val spinner = findViewById<Spinner>(com.example.rainguardapp.R.id.spinner)
+            val adapter: ArrayAdapter<Any?> =
+                ArrayAdapter<Any?>(this, R.layout.simple_spinner_item, ob)
+            adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        fun Select (view:View){
+            val intent = Intent(this@LocationsOB, LocationsCity::class.java)
+            startActivity(intent)
+        }
+
+    }
