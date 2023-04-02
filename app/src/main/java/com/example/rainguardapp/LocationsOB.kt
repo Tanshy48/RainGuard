68f5db3dcefa91642ad91f10b3964bc6a8af13bd
Piwo2L <teslevych.mykhailo@student.uzhnu.edu.ua>
2023-04-02 14:59:43 +0300

    City's

--- app/src/main/java/com/example/rainguardapp/LocationsOB.kt
+++ app/src/main/java/com/example/rainguardapp/LocationsOB.kt
@@ -1,29 +1,78 @@
 package com.example.rainguardapp
 
 import android.R
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
-class LocationsOB:AppCompatActivity(){
-        var ob = arrayOf("Автономна Республіка Крим", "Вінницька", "Волинська", "Дніпропетровська", "Донецька", "Житомирська", "Закарпатська", "Запорізька", "Івано-Франківська", "Київська", "Кіровоградська", "Луганська", "Львівська", "Миколаївська", "Одеська", "Полтавська", "Рівненська", "Сумська", "Тернопільська", "Харківська", "Херсонська", "Хмельницька", "Черкаська", "Чернівецька", "Чернігівська")
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(com.example.rainguardapp.R.layout.locations_ob)
-            val spinner = findViewById<Spinner>(com.example.rainguardapp.R.id.spinner)
-            val adapter: ArrayAdapter<Any?> =
-                ArrayAdapter<Any?>(this, R.layout.simple_spinner_item, ob)
-            adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = adapter
-        }
-        fun Select (view:View){
-            val intent = Intent(this@LocationsOB, LocationsCity::class.java)
-            intent.putExtra("ob", ob)
-            startActivity(intent)
-        }
+class LocationsOB:AppCompatActivity() {
+
+    lateinit var item: String
+    var ob = arrayOf(
+        "Автономна Республіка Крим",
+        "Вінницька",
+        "Волинська",
+        "Дніпропетровська",
+        "Донецька",
+        "Житомирська",
+        "Закарпатська",
+        "Запорізька",
+        "Івано-Франківська",
+        "Київська",
+        "Кіровоградська",
+        "Луганська",
+        "Львівська",
+        "Миколаївська",
+        "Одеська",
+        "Полтавська",
+        "Рівненська",
+        "Сумська",
+        "Тернопільська",
+        "Харківська",
+        "Херсонська",
+        "Хмельницька",
+        "Черкаська",
+        "Чернівецька",
+        "Чернігівська"
+    )
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.rainguardapp.R.layout.locations_ob)
+        val spinner = findViewById<Spinner>(com.example.rainguardapp.R.id.spinner)
+        val adapter: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, R.layout.simple_spinner_item, ob)
+        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        spinner.setAdapter(adapter)
+        val itemSelectedListener: AdapterView.OnItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+
+                     item = parent.getItemAtPosition(position) as String
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
+
+        spinner.onItemSelectedListener = itemSelectedListener
 
     }
+
+    fun Select(view: View) {
+        val intent = Intent(this, LocationsCity::class.java)
+        intent.putExtra("key",item)
+        startActivity(intent)
+    }
+}
