5b13c2dc20ec35e9fdad8811c7a60d115e76b0e3
Tanshy48 <shyshkanych.tetyana@student.uzhnu.edu.ua>
2023-04-09 13:59:45 +0300

    userCheckPermissions

--- app/src/main/java/com/example/rainguardapp/LoginActivity.kt
+++ app/src/main/java/com/example/rainguardapp/LoginActivity.kt
@@ -1,4 +1,15 @@
 package com.example.rainguardapp
 
-class LoginActivity {
+import android.os.Bundle
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+    lateinit var progressBar: ProgressBar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        progressBar = findViewById(R.id.progressBar)
+        progressBar.visibility = ProgressBar.VISIBLE
+    }
 }
--- app/src/main/java/com/example/rainguardapp/MainActivity.kt
+++ app/src/main/java/com/example/rainguardapp/MainActivity.kt
@@ -1,18 +1,121 @@
 package com.example.rainguardapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
-import android.view.View
+import android.provider.Settings
 import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import java.util.*
+import android.location.Location
+import android.widget.TextView
+import com.google.android.gms.location.LocationRequest
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var progressBar: ProgressBar
+    lateinit var city: String
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    val REQUEST_CODE = 101
+    lateinit var lat: TextView
+    lateinit var lon: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         progressBar = findViewById(R.id.progressBar)
         progressBar.visibility = ProgressBar.VISIBLE
+        lat = findViewById(R.id.lat)
+        lon = findViewById(R.id.lon)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        getLocation()
+}
+
+    fun getLocation() {
+        if(checkPermissions()) {
+            if(isLocationEnabled()){
+
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+
+                    return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location?= task.result
+                    if (location == null){
+                        Toast.makeText(this, "немає даних", Toast.LENGTH_SHORT).show()
+                    } else {
+                        lat.text = location.latitude.toString()
+                        lon.text = location.longitude.toString()
+                    }
+                }
+                
+            } else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+
+        } else {
+            requestPermission()
+        }
     }
 
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
+    }
 
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                ||locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun checkPermissions(): Boolean {
+        if(ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            return true
+        }
+        return false
+}
+
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Granted", Toast.LENGTH_SHORT).show()
+                getLocation()
+            } else {
+                Toast.makeText(this,"Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
 }
