6bebbcaec92a8396442752d81d98bc1adade6e8d
Tanshy48 <shyshkanych.tetyana@student.uzhnu.edu.ua>
2023-05-02 23:22:31 +0300

    NewWeatherIcons

--- app/src/main/java/com/example/rainguardapp/FourDaysWeatherCast.kt
+++ app/src/main/java/com/example/rainguardapp/FourDaysWeatherCast.kt
@@ -4,11 +4,9 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
@@ -59,6 +57,7 @@ class FourDaysWeatherCast : AppCompatActivity() {
                 val minTemperatures = Array(5) { "" }
                 val maxTemperatures = Array(5) { "" }
                 val descriptions = Array(5) { "" }
+                val imgsrc = Array(5){""}
 
                 val jsonObj = JSONObject(result)
                 val weatherList = jsonObj.getJSONArray("list")
@@ -80,6 +79,7 @@ class FourDaysWeatherCast : AppCompatActivity() {
                     val weather = weatherArray.getJSONObject(0)
                     val description = weather.getString("description")
                     descriptions[i] = description.toString()
+                    imgsrc[i] = "https://openweathermap.org/img/wn/${weather.getString("icon")}.png"
                 }
 
 
@@ -87,28 +87,47 @@ class FourDaysWeatherCast : AppCompatActivity() {
                 findViewById<TextView>(R.id.date1).text = datesString[0]
                 findViewById<TextView>(R.id.status1).text = descriptions[0]
                 findViewById<TextView>(R.id.temp_min_max1).text = "Min Temp: " + minTemperatures[0] + "°C" + "/Max Temp: " + maxTemperatures[0] +"°C"
-
+                val imageView1 = findViewById<ImageView>(R.id.icon1)
+                Glide.with(this@FourDaysWeatherCast)
+                    .load(imgsrc[0])
+                    .into(imageView1)
 
                 findViewById<TextView>(R.id.avgTemp2).text = averageTemperatures[1]
                 findViewById<TextView>(R.id.date2).text = datesString[1]
                 findViewById<TextView>(R.id.status2).text = descriptions[1]
                 findViewById<TextView>(R.id.temp_min_max2).text = "Min Temp: " + minTemperatures[1] + "°C" + "/Max Temp: " + maxTemperatures[1] +"°C"
+                val imageView2 = findViewById<ImageView>(R.id.icon2)
+                Glide.with(this@FourDaysWeatherCast)
+                    .load(imgsrc[1])
+                    .into(imageView2)
 
                 findViewById<TextView>(R.id.avgTemp3).text = averageTemperatures[2]
                 findViewById<TextView>(R.id.date3).text = datesString[2]
                 findViewById<TextView>(R.id.status3).text = descriptions[2]
                 findViewById<TextView>(R.id.temp_min_max3).text = "Min Temp: " + minTemperatures[2] + "°C" + "/Max Temp: " + maxTemperatures[2] +"°C"
+                val imageView3 = findViewById<ImageView>(R.id.icon3)
+                Glide.with(this@FourDaysWeatherCast)
+                    .load(imgsrc[2])
+                    .into(imageView3)
+
 
                 findViewById<TextView>(R.id.avgTemp4).text = averageTemperatures[3]
                 findViewById<TextView>(R.id.date4).text = datesString[3]
                 findViewById<TextView>(R.id.status4).text = descriptions[3]
                 findViewById<TextView>(R.id.temp_min_max4).text = "Min Temp: " + minTemperatures[3] + "°C" + "/Max Temp: " + maxTemperatures[3] +"°C"
+                val imageView4 = findViewById<ImageView>(R.id.icon4)
+                Glide.with(this@FourDaysWeatherCast)
+                    .load(imgsrc[3])
+                    .into(imageView4)
 
                 findViewById<TextView>(R.id.avgTemp5).text = averageTemperatures[4]
                 findViewById<TextView>(R.id.date5).text = datesString[4]
                 findViewById<TextView>(R.id.status5).text = descriptions[4]
                 findViewById<TextView>(R.id.temp_min_max5).text = "Min Temp: " + minTemperatures[4] + "°C" + "/Max Temp: " + maxTemperatures[4] +"°C"
-
+                val imageView5 = findViewById<ImageView>(R.id.icon5)
+                Glide.with(this@FourDaysWeatherCast)
+                    .load(imgsrc[4])
+                    .into(imageView5)
 
                 findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
                 findViewById<LinearLayout>(R.id.mainContainer).visibility = View.VISIBLE
