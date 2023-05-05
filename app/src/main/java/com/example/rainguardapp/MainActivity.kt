4ee8cdd836865a679db23350ff5521b020ae5429
Tanshy48 <shyshkanych.tetyana@student.uzhnu.edu.ua>
2023-05-05 14:28:20 +0300

    Fixed main page design

--- app/src/main/java/com/example/rainguardapp/MainActivity.kt
+++ app/src/main/java/com/example/rainguardapp/MainActivity.kt
@@ -16,6 +16,7 @@ import org.json.JSONObject
 import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     lateinit var CITY: String
@@ -27,6 +28,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var temp:String
     lateinit var tempMax:String
     lateinit var tempMin:String
+    var updatedAt by Delegates.notNull<Long>()
+    var sunrise by Delegates.notNull<Long>()
+    var sunset by Delegates.notNull<Long>()
+    lateinit var updatedAtText: String
+    lateinit var sunriseText: String
+    lateinit var sunsetText: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -99,20 +107,32 @@ class MainActivity : AppCompatActivity() {
                 val main = jsonObj.getJSONObject("main")
                 val sys = jsonObj.getJSONObject("sys")
                 val wind = jsonObj.getJSONObject("wind")
+                updatedAt = jsonObj.getLong("dt")
+                sunrise = sys.getLong("sunrise")
+                sunset = sys.getLong("sunset")
                 when (value){
                     "0" -> {
+                        updatedAtText = SimpleDateFormat("dd/MM/yyyy HH:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                        sunsetText = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                        sunriseText = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
                         windSpeed = " " + wind.getString("speed")+" м/s"
                         temp = " " + main.getString("temp") + "°C"
                         tempMin = " " + main.getString("temp_min") + "°C"
                         tempMax = " " + main.getString("temp_max") + "°C"
                     }
                     "1" -> {
+                        updatedAtText = SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                        sunsetText = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                        sunriseText = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
                         windSpeed = " " + wind.getString("speed")+"ft/s"
                         temp = " " + main.getString("temp") + "°F"
                         tempMin =  " " + main.getString("temp_min") + "°F"
                         tempMax =  " " + main.getString("temp_max") + "°F"
                     }
                     else -> {
+                        updatedAtText = SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                        sunsetText = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                        sunriseText = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
                         windSpeed = " " + wind.getString("speed") + "м/s"
                         temp = " " + main.getString("temp") + "°C"
                         tempMin = " " + main.getString("temp_min") + "°C"
@@ -120,24 +140,18 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
-                val updatedAt:Long = jsonObj.getLong("dt")
-                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
                 val pressure = main.getString("pressure")
                 val humidity = main.getString("humidity")
-                val sunrise:Long = sys.getLong("sunrise")
-                val sunset:Long = sys.getLong("sunset")
-                val weatherDescription = weather.getString("description")
                 val address = jsonObj.getString("name")+", "+sys.getString("country")
                 val imgsrc = "https://openweathermap.org/img/wn/${weather.getString("icon")}.png"
                 /* Populating extracted data into our views */
                 findViewById<TextView>(R.id.address).text = address
                 findViewById<TextView>(R.id.updated_at).text =  updatedAtText
-                findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
                 findViewById<TextView>(R.id.temp).text = temp
                 findViewById<TextView>(R.id.temp_min1).text = tempMin
                 findViewById<TextView>(R.id.temp_max1).text = tempMax
-                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
-                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                findViewById<TextView>(R.id.sunrise).text = sunriseText
+                findViewById<TextView>(R.id.sunset).text = sunsetText
                 findViewById<TextView>(R.id.wind).text = windSpeed
                 findViewById<TextView>(R.id.pressure).text = pressure
                 findViewById<TextView>(R.id.humidity).text = humidity
